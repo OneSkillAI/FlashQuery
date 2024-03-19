c84cbe7bb0be4befbd3fdac76afc7118a5b09fcd
OneSkillAI <163226834+OneSkillAI@users.noreply.github.com>
2024-03-19 21:58:39 +1100

    Update chat-menu-header.tsx

--- src/features/chat-page/chat-menu/chat-menu-header.tsx
+++ src/features/chat-page/chat-menu/chat-menu-header.tsx
@@ -1,8 +1,3 @@
 import { CreateChatAndRedirect } from "../chat-services/chat-thread-service";
 import { ChatContextMenu } from "./chat-context-menu";
 import { NewChat } from "./new-chat";
-
-export const ChatMenuHeader = () => {
-  return (
-  );
-};
