5ee101392f0a3614390e18a91d1eadcdb040aaee
OneSkillAI <163226834+OneSkillAI@users.noreply.github.com>
2024-04-15 16:09:35 +1000

    Update persona-view.tsx

--- src/features/persona-page/persona-card/persona-view.tsx
+++ src/features/persona-page/persona-card/persona-view.tsx
@@ -21,15 +21,15 @@ interface Props {
 export const ViewPersona: FC<Props> = (props) => {
   const { persona } = props;
   const { data } = useSession();
-
-
   const isAdmin = data?.user?.isAdmin || false; 
+  const isFlashQuery = persona.name === "FlashQuery"; 
 
-
+ 
   const defaultMessage = "Only administrators can see the instructions for this assistant";
-  const textareaDefaultValue = !isAdmin && persona.name === "FlashQuery" 
+  const textareaDefaultValue = (isFlashQuery && !isAdmin) 
                                ? defaultMessage 
                                : persona.personaMessage;
+
 
   return (
     <Sheet>
