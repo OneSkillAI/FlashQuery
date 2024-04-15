a566ad1ce533e04f34a312951bec73f1f04a9297
OneSkillAI <163226834+OneSkillAI@users.noreply.github.com>
2024-04-15 15:45:11 +1000

    Update persona-view.tsx

--- src/features/persona-page/persona-card/persona-view.tsx
+++ src/features/persona-page/persona-card/persona-view.tsx
@@ -20,6 +20,7 @@ interface Props {
 
 export const ViewPersona: FC<Props> = (props) => {
   const { persona } = props;
+  const { data } = useSession();
   const isAdmin = data?.user?.isAdmin || false;
   const isFlashQuery = persona.name === "FlashQuery";
   return (
